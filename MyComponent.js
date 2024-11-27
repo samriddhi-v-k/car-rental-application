82b40a2404933de9115fa4617fe934ce90f48964
samriddhi-v-k <samriddhivk17@gmail.com>
2024-11-27 22:48:04 +0530

    Add files via upload

---
+++ MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>hello this is my first react component</h1>
+        </div>
+    );
+};
+export default MyComponent;
+
